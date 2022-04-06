2b04b827959ddc24bfd3967cbf2db9692b0310c7
Phi Thach <lephithach00@gmail.com>
2022-04-06 19:53:52 +0700

    first commit

---
+++ js/function.js
@@ -0,0 +1,32 @@
+const kickContainerList = document.querySelectorAll(".kick-container .kick");
+
+const playAudio = (element) => {
+  element.currentTime = 0;
+  element.play();
+};
+
+kickContainerList.forEach((kick) => {
+  kick.onclick = (e) => {
+    playAudio(kick.querySelector("audio"));
+  };
+});
+
+const snareContainerList = document.querySelectorAll(".snare-container .snare");
+
+snareContainerList.forEach((snare) => {
+  snare.onclick = (e) => {
+    playAudio(snare.querySelector("audio"));
+  };
+});
+
+const clapContainerList = document.querySelectorAll(".clap-container .clap");
+
+clapContainerList.forEach((clap) => {
+  clap.onclick = (e) => {
+    playAudio(clap.querySelector("audio"));
+  };
+});
+
+window.addEventListener("keydown", (e) => {
+  console.log(e.which);
+});
